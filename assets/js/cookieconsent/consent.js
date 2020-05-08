4cc2638c43b212b32b848f3ce05f26efb926d4a8
Jose Manuel Gil <jmgil>
2020-05-08 15:58:39 +0200

    Create consent.js

---
+++ assets/js/cookieconsent/consent.js
@@ -0,0 +1,14 @@
+window.cookieconsent.initialise({
+    "palette": {
+      "popup": {
+        "background": "#383b75"
+      },
+      "button": {
+        "background": "#f1d600"
+      }
+    },
+    "theme": "edgeless",
+    "content": {
+      "message": "We use cookies to personalise content and ads, to provide social media features and to analyse our traffic. We also share information about your use of our site with our social media, advertising and analytics partners who may combine it with other information that you’ve provided to them."
+    }
+});
